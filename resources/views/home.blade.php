11c0f2256363ca0fe6dcce4333807ec7858491ef
Chandramouli Parasuraman <chandramouli.parasuraman@aspiresys.com>
2016-09-09 07:52:34 +0530

    Make minor style changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,17 +3,17 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            Welcome to Safe Buddy Application 
-    </div>
-    <div class="col-sm">
-        <div class="service text-center">
-          <div class="icon"> <img alt="" src="/logo_SafeBuddy.png"> </div>
-          <h4>Track</h4>
-          <p>Lookup a quick <i>tracking</i> of all your employees availing cab facility at nights to ensure that they reach home safe.</p>
+        <div class="col-md-8 col-md-offset-2" style="text-align:center;font-weight:800">
+            <h1>Welcome to Safe Buddy Application</h1>
         </div>
+        <div class="col-sm">
+            <div class="service text-center">
+              <div class="icon"> <img alt="" src="/logo_SafeBuddy.png"> </div>
+              <h2 style="margin:1em">Track</h2>
+              <p>Do a quick <i>tracking</i> of all your employees availing cab facility at nights to ensure that they reach home safe.</p>
+            </div>
         <!--/.service -->
-      </div>
-</div>
+        </div>
+    </div>
 </div>
 @endsection
